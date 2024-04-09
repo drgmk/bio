c151564d55ee39e97128e68f9e5efef36efe0c45
drgmk <grantkennedy@gmail.com>
2024-04-09 13:40:34 +1200

    updating for images within files

--- imaging/watcher.py
+++ imaging/watcher.py
@@ -1,30 +1,91 @@
-"""Script to plot new images in a watched folder,
-giving an idea of whether they are saturated or not."""
+"""
+Script to plot new images in a watched folder/file, indicating whether they are saturated or not.
+
+The Leica software appears to hold the images in memory and will write them to disk when requested
+(not clear that these are in a tmp folder/file). A user could therefore capture many images before
+a file is written out.
+
+Workflow is therefore that user takes images and saves as they go, which updates the file. The script
+periodically (~1min) reads the file and plots the latest set.
+
+run with:
+py "C:\\Program Files\\python_scripts\\watcher.py" "Z:\\Image Facility User Data\\User\\Folder"
+"""
 
 import glob
 import sys
 import os
 import time
-from readlif.reader import LifFile
 import numpy as np
 import matplotlib.pyplot as plt
+from readlif.reader import LifFile
+
+
+def get_meta(et, name, i):
+    """Return the channel metadata given a element tree of all input data.
+
+    Unclear what settings actually correspond to what we want, but can
+    explore by viewing xml tree and comparing to what LAS X gives.
+    """
+    # print(f'meta for {name}')
+    meta = {'name': name}
+    data = et.find(f".//Element[@Name='{name}']")
+
+    # wavelength (not all channels have line)
+    # x = data.findall(".//Attachment[@Name='HardwareSetting']/ATLConfocalSettingDefinition//DetectionReferenceLine")
+    # wav_int = []
+    # for x_ in x:
+    #     wav_int.append(x_.attrib['LaserWavelength'])
+    # meta['wav'] = wav_int[i]
+
+    # dye for each channel
+    dyes = []
+    x = data.findall(".//ChannelDescription/ChannelProperty")
+    for j in x:
+        for k in j:
+            if k.text == 'DyeName':
+                dyes.append(j[1].text)
+
+    meta['dye'] = dyes[i]
+
+    # timestamps
+    x = data.find(".//TimeStampList")
+    times = [float.fromhex(a) for a in x.text.strip().split(' ')]
+    meta['time'] = times[i]
+
+    return meta
+
+
+def get_last_image(file):
+    f = LifFile(file)
+
+    times = []
+    for i in range(f.num_images):
+        im = f.get_image(i)
+        meta = get_meta(f.xml_root, im.info['name'], 0)
+        times.append(meta['time'])
+
+    return np.argmax(times)
 
 
 def plot(file, fig, max=255):
-    """Plot panels of a LIF file, with histograms."""
+    """Plot channels of a LIF file image, with histograms."""
 
     # open the file and get frames
     # could reduce image sizes if necessary
+    last_i = get_last_image(file)
     lif = LifFile(file)
+    im = lif.get_image(last_i)
+    print(f"plotting file: {file}, {im.info['name']}")
     ims = []
     meta = []
-    for i, im in enumerate(lif.get_iter_image()):
-        ims.append(im.get_frame())
-        meta.append(im.info['name'])
+    for i, ch in enumerate(im.get_iter_c()):
+        ims.append(ch)
+        meta.append(get_meta(lif.xml_root, im.info['name'], i))
 
     # clear the last figure and set up the grid
     fig.clf()
-    grid = fig.add_gridspec(2, int(len(ims)//2), hspace=0.2)
+    grid = fig.add_gridspec(2, int(np.ceil(len(ims)/2)), hspace=0.2)
 
     # loop over the images and plot them plus their histograms
     for i in range(len(ims)):
@@ -34,7 +95,7 @@ def plot(file, fig, max=255):
         ls = [max-1, max+1]
         axs[0].imshow(ims[i], vmax=np.percentile(data, 99), cmap='Greys')
         axs[0].contour(ims[i], levels=ls, colors='red')
-        axs[0].set_title(meta[i])
+        axs[0].set_title(meta[i]['dye'])
 
         _ = axs[1].hist(data, bins=100, log=True)
         axs[1].text(max, np.max(_[0]), f'{np.sum(data==max)/len(data)*100:3.2f}% sat',
@@ -57,22 +118,23 @@ if __name__ == '__main__':
 
     fig = plt.figure(figsize=(10, 6))
 
-    last_file = ''
+    last_mtime = 0
     while True:
 
-        time.sleep(1)
+        time.sleep(10)
 
         fs = np.array(glob.glob(f'{sys.argv[1]}/*lif'))
 
-        mtimes = []
+        mtimes = np.array([])
         for f in fs:
-            mtimes.append(os.path.getmtime(f))
+            mtimes = np.append(mtimes, os.path.getmtime(f))
 
         srt = np.argsort(mtimes)
         fs = fs[srt]
+        mtimes = mtimes[srt]
 
-        if fs[-1] != last_file:
+        if mtimes[-1] != last_mtime:
             print(f'Showing: {fs[-1]}')
             plot(fs[-1], fig)
 
-        last_file = fs[-1]
+        last_mtime = mtimes[-1]
